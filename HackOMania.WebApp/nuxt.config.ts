e23038e8823f2e6f8bf4d3139550f064142410df
Qin Guan <qinguan20040914@gmail.com>
2026-01-30 18:05:44 +0800

    Script

--- HackOMania.WebApp/nuxt.config.ts
+++ HackOMania.WebApp/nuxt.config.ts
@@ -10,11 +10,6 @@ export default defineNuxtConfig({
     '@nuxt/fonts',
   ],
 
-  colorMode: {
-    preference: 'light',
-    fallback: 'light',
-  },
-
   ssr: false,
 
   // allows auto-import for constants
@@ -22,8 +17,29 @@ export default defineNuxtConfig({
     dirs: ['composables/constants'],
   },
   devtools: { enabled: true },
+  app: {
+    head: {
+      script: [
+        {
+          type: 'text/javascript',
+          innerHTML: `
+              (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "v9hg1hk686");
+          `,
+        },
+      ],
+    },
+  },
 
   css: ['~/assets/css/main.css'],
+
+  colorMode: {
+    preference: 'light',
+    fallback: 'light',
+  },
 
   runtimeConfig: {
     public: {

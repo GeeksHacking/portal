09e0368d4b13f22e0f0da07a930d912a8e203b7b
julwrites <mail@tehj.io>
2026-02-01 09:37:54 +0800

    Adding prog team admin

--- HackOMania.Infra/Stacks/DefaultStack.cs
+++ HackOMania.Infra/Stacks/DefaultStack.cs
@@ -337,6 +337,16 @@ public class DefaultStack : Stack
                                 },
                                 new ServiceTemplateContainerEnvArgs
                                 {
+                                    Name = "App__AdminGitHubLogins__11",
+                                    Value = "tiramisuxh",
+                                },
+                                new ServiceTemplateContainerEnvArgs
+                                {
+                                    Name = "App__AdminGitHubLogins__12",
+                                    Value = "misterdoobdoob",
+                                },
+                                new ServiceTemplateContainerEnvArgs
+                                {
                                     Name = "GitHub__ClientId",
                                     ValueSource = new ServiceTemplateContainerEnvValueSourceArgs
                                     {

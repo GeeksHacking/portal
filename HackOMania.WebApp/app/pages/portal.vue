f8dca60f21fae80b81773150531c3213ac75a94c
tohkailiang <tohkailiang@gmail.com>
2026-03-05 15:24:05 +0800

    placeholder timeline

--- HackOMania.WebApp/app/pages/portal.vue
+++ HackOMania.WebApp/app/pages/portal.vue
@@ -7,6 +7,9 @@
     <div id="challenges" class="scroll-mt-12 lg:scroll-mt-18">
       <PortalChallengesSection />
     </div>
+    <!-- <div id="timeline" class="scroll-mt-12 lg:scroll-mt-18">
+      <PortalTimelineSection />
+    </div> -->
     <div id="team" class="scroll-mt-12 lg:scroll-mt-18">
       <PortalTeamSection />
     </div>
